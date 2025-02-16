d84438957e16f7452d2aaf7c1602cfc9f702cbd8
kervint1 <s1f102302124@iniad.org>
2025-02-16 12:14:38 +0900

    下線

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,6 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import './Navigation.css'; // 新しいCSSファイルをインポート
 
 function Navigation() {
+  const location = useLocation();
+  const getLinkClass = (path) => location.pathname === path ? 'nav-link text-dark border-bottom-thick' : 'nav-link text-dark';
+
   return (
     <nav className="navbar navbar-expand-md bg-white border-bottom shadow-sm fixed-top">
       <div className="container">
@@ -24,25 +28,25 @@ function Navigation() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link text-dark" to="/">Home</Link>
+              <Link className={getLinkClass("/")} to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-dark" to="/calendar">カレンダー</Link>
+              <Link className={getLinkClass("/calendar")} to="/calendar">カレンダー</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-dark" to="/receipt-registration">支出</Link>
+              <Link className={getLinkClass("/receipt-registration")} to="/receipt-registration">支出</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-dark" to="/receipt-income">収入</Link>
+              <Link className={getLinkClass("/receipt-income")} to="/receipt-income">収入</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-dark" to="/registration-history">登録履歴</Link>
+              <Link className={getLinkClass("/registration-history")} to="/registration-history">登録履歴</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-dark" to="/income-history">収入履歴</Link>
+              <Link className={getLinkClass("/income-history")} to="/income-history">収入履歴</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-dark" to="/graph">収支グラフ</Link>
+              <Link className={getLinkClass("/graph")} to="/graph">収支グラフ</Link>
             </li>
           </ul>
         </div>
